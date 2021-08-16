01e70cb260a08d84cb6ef69311f9ed956f5b353f
LeoMurphyWM24 <ltmurphy@email.wm.edu>
2021-08-16 10:00:01 -0400

    Read and decompress Isat data using 'Scale' and 'Offset' header attributes; code slowness to be addressed

--- getIsat.py
+++ getIsat.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 import numpy as np
 import xarray as xr
 import astropy.units as u
@@ -5,6 +7,7 @@ import astropy.units as u
 from hdf5reader import *
 
 # Note: This code is based heavily on similar code written by the same author for the LAPD plasma analysis repository.
+# MAKE GET_ISWEEP_VSWEEP A NAMESPACE PACKAGE WITH GENERIC DATA TO ACCESS AND IMPORT HERE
 
 
 def get_isat(filename):
@@ -12,6 +15,11 @@ def get_isat(filename):
     hdf5_file = open_hdf5(filename)
     x_round, y_round, shot_list = get_mach_xy(hdf5_file)
     xy_shot_ref, x, y = categorize_mach_xy(x_round, y_round, shot_list)
+
+    mach_data, mach_headers = get_mach_data_headers(hdf5_file)
+
+    # print("Decompressing mach data...")
+    scale_offset_decompress(mach_data, mach_headers)
 
     isat_array = None
 
@@ -69,6 +77,11 @@ def categorize_mach_xy(x_round, y_round, shot_list):  # Taken from LAPD getIVswe
         # noinspection PyTypeChecker
         xy_shot_ref[x_loc[i]][y_loc[i]].append(i)  # full of references to nth shot taken
 
+    """
+    Should I rewrite getIVsweep.py in other project to make it able to read any data of user's choice, then use it
+        to collect bias and current data in lapd-plasma-analysis and Isat in lapd-mach-probe?
+    """
+
     return xy_shot_ref, x, y
 
     # st_data = []
@@ -76,5 +89,54 @@ def categorize_mach_xy(x_round, y_round, shot_list):  # Taken from LAPD getIVswe
     # This part: (# unique x positions * # unique y positions) grid storing location? of shot numbers at that position
     # SKIP REMAINING X, Y POSITION DATA PROCESSING
 
-# TODO Should I rewrite getIVsweep.py in other project to make it able to read any data of user's choice, then use it
-#  to collect bias and current data in lapd-plasma-analysis and Isat in lapd-mach-probe?
+
+def get_mach_data_headers(hdf5_file):
+
+    # SIS crate data
+    # sis_group = structures_at_path(hdf5_file, '/Raw data + config/SIS crate/')
+    sis_data = structures_at_path(hdf5_file, '/Raw data + config/SIS crate/')['Datasets']
+    # print("Datasets in sis_data structure: " + str(sis_group["Datasets"]))
+
+    # pprint(sis_data, width=120)
+
+    # Mach probe has 6 faces; data are in slot numbers 12, 14, ..., 22 and corresponding headers in numbers 13, ..., 23
+    mach_data_paths = [sis_data[12 + (2 * i)] for i in range(6)]
+    mach_headers_paths = [sis_data[12 + (2 * i + 1)] for i in range(6)]
+    mach_data_raw = np.array([hdf5_file[path] for path in mach_data_paths])
+    mach_headers_raw = np.array([hdf5_file[path] for path in mach_headers_paths])
+
+    # print(mach_headers_raw.dtype)
+    # print(mach_headers_raw['Scale'])
+
+    # print("Shape of mach data array:", mach_data_raw.shape, "and headers array:", mach_headers_raw.shape)
+
+    return mach_data_raw, mach_headers_raw
+
+
+def scale_offset_decompress(data_raw, data_headers):  # change INPUT PARAMETERS and DIMENSION OF INPUT
+    r"""
+    Decompress raw data using the specified arrays of scales and offsets.
+    Scale and offset arrays must have first dimension corresponding to the length of the input data
+    (for example, the number of shots taken).
+
+    Parameters
+    ----------
+    :param data_raw: array
+    :param data_headers: structured array
+    :return: decompressed data array
+    """
+
+    scales = data_headers['Scale']
+    offsets = data_headers['Offset']
+
+    if len(data_raw.shape) not in [2, 3]:
+        raise ValueError("Only 2D or 3D arrays are currently supported for data decompression.")
+
+    if data_raw.shape[:-1] != data_headers.shape:
+        raise ValueError("Data and headers have incompatible shapes and cannot be broadcast together")
+
+    # Reshape scales and offsets to have same dimensions as raw data, except with last dimension one to allow broadcast
+    scales = scales.reshape(*data_raw.shape[:-1], 1)
+    offsets = offsets.reshape(*data_raw.shape[:-1], 1)
+        
+    return data_raw * scales + offsets
